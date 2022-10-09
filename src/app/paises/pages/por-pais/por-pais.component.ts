bc66bce791fbb4eb756806a76b8c054094837ef0
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-10-09 00:12:01 -0500

    crear HTML del componente por-pais y agregar FormModule de Angular para recibir info de nuestro input

--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./por-pais.component.css']
 })
 export class PorPaisComponent implements OnInit {
-
+  termino: string = 'Hola Mundo';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar(){
+    console.log(this.termino)
   }
 
 }
