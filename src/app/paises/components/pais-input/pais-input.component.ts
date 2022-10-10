328aa88b24cd9f957e52c1c1c535f7d411dfdbce
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-10-09 22:27:56 -0500

    crear un debounceTime

--- src/app/paises/components/pais-input/pais-input.component.ts
+++ src/app/paises/components/pais-input/pais-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pais-input',
@@ -8,15 +9,29 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class PaisInputComponent implements OnInit {
 
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer: Subject<string> = new Subject();
 
   termino: string = '';
   
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.debouncer
+    .pipe(
+      debounceTime(300))
+    .subscribe( valor => {
+      this.onDebounce.emit(valor);
+      // console.log('debouncer', valor);
+    })
   }
   buscar(){
    this.onEnter.emit(this.termino);
+  }
+  teclaPresionada(){
+   this.debouncer.next( this.termino );
+
   }
 
 }
